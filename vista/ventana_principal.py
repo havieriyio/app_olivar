9cbbecafeaed73cfe4f0a8aec48657e46b0593f6
javiergalean@gmail.com <javiergalean@gmail.com>
2025-06-16 22:33:35 +0200

    version estable 10

--- vista/ventana_principal.py
+++ vista/ventana_principal.py
@@ -41,22 +41,47 @@ class VentanaPrincipal(tk.Tk):
         self.contenedor_panel = tk.Frame(self)
         self.contenedor_panel.pack(fill='both', expand=True)
 
+        self.frame_parcelas = None
+        self.frame_analisis = None
+
         self.main_frame = tk.Frame(self, bg="white")
         self.main_frame.pack(side="right", expand=True, fill="both")
 
-    def abrir_gestion_explotaciones(self):
-        ExplotacionViewer(self)
-
+    
     def abrir_gestion_parcelas(self):
-        for widget in self.contenedor_panel.winfo_children():
-            widget.destroy()
+        self.ocultar_frames()
 
-        # Crea e inserta el nuevo panel
-        self.panel_parcela = ParcelaViewer(self.contenedor_panel)
-        self.panel_parcela.pack(fill='both', expand=True)
+        # Siempre crea una nueva instancia para que las parcelas reflejen la explotación activa
+        self.frame_parcelas = ParcelaViewer(self.contenedor_panel)
+        self.frame_parcelas.pack(fill='both', expand=True)
 
+        
+            
     def abrir_gestion_analisis(self):
-        AnalisisViewer(self)
+        self.ocultar_frames()
+
+        # Crear siempre una nueva instancia garantiza que los datos se actualicen
+        self.frame_analisis = AnalisisViewer(self.contenedor_panel)
+        self.frame_analisis.pack(fill='both', expand=True)
+
+        
+    def ocultar_frames(self):
+        for widget in self.contenedor_panel.winfo_children():
+            widget.pack_forget()
+
+    def abrir_gestion_explotaciones(self):
+        def on_close():
+            self.actualizar_encabezado()
+
+            # Borra las vistas para forzar su recarga tras cambio de explotación
+            self.frame_parcelas = None
+            self.frame_analisis = None
+
+            self.abrir_gestion_parcelas()
+
+        ventana = ExplotacionViewer(self)
+        ventana.protocol("WM_DELETE_WINDOW", lambda: [ventana.destroy(), on_close()])
+
 
     def actualizar_encabezado(self):
         usuario = getattr(self, "usuario", "Desconocido")
